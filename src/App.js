042476e8f5d37ee557a30c0a46d469241a74de88
Thomas Collardeau <thomas@collardeau.com>
2017-07-28 20:30:27 +0200

    add styles

--- src/App.js
+++ src/App.js
@@ -8,7 +8,13 @@ const AudioContext = window.AudioContext || window.webkitAudioContext;
 const audioCtx = new AudioContext();
 
 const PlayArea = ({ children, setGain }) =>
-  <div onMouseLeave={() => setGain(0)}>
+  <div
+    onMouseLeave={() => setGain(0)}
+    style={{
+      backgroundColor: '#ccc',
+      padding: '0 1vw'
+    }}
+  >
     {children}
   </div>;
 
@@ -26,9 +32,24 @@ const App = compose(
 )(props => {
   const { gain, frequency, setGain, setFrequency } = props;
   return (
-    <main>
+    <main
+      style={{
+        maxWidth: '800px',
+        margin: '0 auto'
+      }}
+    >
       <Oscillator audioCtx={audioCtx} gain={gain} frequency={frequency} />
       <h3>Musicolor</h3>
+      <div style={{ fontSize: '2em', marginBottom: '2vh' }}>
+        <select
+          style={{
+            width: '300px',
+            height: '50px'
+          }}
+        >
+          <option>Chromatic Scale (All 12 notes)</option>
+        </select>
+      </div>
       <PlayArea setGain={setGain}>
         <NeutralZone />
         <Theramin start={110} setFrequency={setFrequency} onEnter={() => setGain(0.5)} />
