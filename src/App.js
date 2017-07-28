6a345249dcab686a4a936fe173a958009c523479
Thomas Collardeau <thomas@collardeau.com>
2017-07-28 18:55:59 +0200

    update app to use new comps

--- src/App.js
+++ src/App.js
@@ -1,52 +1,31 @@
 import React from 'react';
-import { compose, withProps } from 'recompose';
+import { compose, withState } from 'recompose';
+import Oscillator from './components/Oscillator';
+import Theramin from './components/Theramin';
 import './App.css';
 
 const AudioContext = window.AudioContext || window.webkitAudioContext;
 const audioCtx = new AudioContext();
-const oscillator = audioCtx.createOscillator();
-const gainNode = audioCtx.createGain();
-gainNode.gain.value = 0;
-oscillator.frequency.value = 110;
-oscillator.connect(gainNode);
-gainNode.connect(audioCtx.destination);
-oscillator.start();
 
-const enhance = compose(
-  withProps({
-    changeTone: hz => {
-      oscillator.frequency.value = hz;
-    },
-    changeGain: value => {
-      gainNode.gain.value = value;
-    },
-    mute: () => {
-      gainNode.gain.value = 0;
-    }
-  })
-);
+const PlayArea = ({ children, setGain }) =>
+  <div onMouseEnter={() => setGain(0.5)} onMouseLeave={() => setGain(0)}>
+    {children}
+  </div>;
 
-const App = enhance(({ changeTone, changeGain, mute }) =>
-  <div>
-    <h3>Theramin</h3>
-    <div>Mouse from left to right for higher pitches</div>
-    <div
-      style={{
-        width: '100vw',
-        height: '100px',
-        border: '1px solid black'
-      }}
-      onMouseEnter={e => {
-        changeGain(0.2);
-      }}
-      onMouseMove={e => {
-        changeTone(e.clientX);
-      }}
-      onMouseLeave={() => {
-        mute();
-      }}
-    />
-  </div>
-);
+const App = compose(
+  withState('gain', 'setGain', 0),
+  withState('frequency', 'setFrequency', 110)
+)(props => {
+  const { gain, frequency, setGain, setFrequency } = props;
+  return (
+    <main>
+      <Oscillator audioCtx={audioCtx} gain={gain} frequency={frequency} />
+      <h3>Musicolor</h3>
+      <PlayArea setGain={setGain}>
+        <Theramin start={110} changeFrequency={setFrequency} />
+      </PlayArea>
+    </main>
+  );
+});
 
 export default App;
